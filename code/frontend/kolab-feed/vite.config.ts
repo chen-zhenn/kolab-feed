a918f79e0c78e52bc043f6580b4d430867ec4075
Carlos Garcia <chenzhen.dev@gmail.com>
2025-01-06 18:16:17 -0300

    chore: update vite setup

--- code/frontend/kolab-feed/vite.config.ts
+++ code/frontend/kolab-feed/vite.config.ts
@@ -1,13 +1,34 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': '/src',
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  const baseUrl = `http://${env.VITE_APP_HOST}:${env.VITE_APP_PORT}`;
+
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
+    },
+    define: {
+      __APP_ENV__: JSON.stringify(env.APP_ENV),
+      __VITE_API_BASE_URL__: JSON.stringify(env.VITE_API_BASE_URL),
+      __VITE_SUPABASE_URL__: JSON.stringify(env.VITE_SUPABASE_URL),
+      __VITE_SUPABASE_KEY__: JSON.stringify(env.VITE_SUPABASE_KEY),
+      __VITE_APP_BASE_URL__: JSON.stringify(baseUrl),
+    },
+    server: {
+      port: Number(env.VITE_APP_PORT) || 3000,
+      host: env.VITE_APP_HOST || 'localhost',
+    },
+    build: {
+      sourcemap: true,
+      outDir: 'dist', 
+      rollupOptions: {}
     }
-  }
-  
-})
+  };
+});
